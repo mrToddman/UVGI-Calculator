15f15479a3f83a5aecc2a01ce85e969496f71b72
Todd Miller <48498389+mrToddman@users.noreply.github.com>
2023-01-13 02:06:15 -0800

    Delete UVirradianceXY.m

--- UVirradianceXY.m
+++
@@ -1,9 +0,0 @@
-function [E]=UVirradianceXY(ductWidth,ductHieght,xLampPosition,yLampPosition,lampLength,phi)
-
-x=0.01:0.01:(ductWidth);
-y=0.01:0.01:(ductHieght);
-[x,y]=meshgrid(x,y);
-
-a=((x-xLampPosition).^2 + (y-yLampPosition).^2).^(0.5);
-E=UVirradianceR(a,lampLength,phi);
-E(isnan(E))=0;
